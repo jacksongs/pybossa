0316beb84fc401c6ab77e1cd872df319e127ce04
jacksongs <jacksongs@gmail.com>
2017-08-26 20:16:37 +1000

    Update

--- pybossa/jobs.py
+++ pybossa/jobs.py
@@ -346,6 +346,8 @@ def get_autoimport_jobs(queue='low'):
         current_app.logger.error("v3b")
         projects = (p.dictize() for p in project_repo.get_all())
     current_app.logger.error("v4")
+    current_app.logger.error(str(len(project_repo.get_all())))
+    current_app.logger.error(str(len(projects)))
     for project_dict in projects:
         current_app.logger.error("v5")
         project = project_repo.get(project_dict['id'])
