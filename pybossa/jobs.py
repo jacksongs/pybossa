a4246230d90fb2102d158a35d4e9f2ffde146d18
jacksongs <jacksongs@gmail.com>
2017-08-26 16:14:07 +1000

    Updated logging for auto import

--- pybossa/jobs.py
+++ pybossa/jobs.py
@@ -317,7 +317,9 @@ def get_non_contributors_users_jobs(queue='quaterly'):
 
 def get_autoimport_jobs(queue='maintenance'):
     """Get autoimport jobs."""
-    print "Autoimport running at",datetime.datetime.now()
+    msg = ("Autoimporting jobs at",datetime.datetime.now())
+    app = current_app
+    app.logger.warning(msg)
     from pybossa.core import project_repo
     import pybossa.cache.projects as cached_projects
     from pybossa.pro_features import ProFeatureHandler
